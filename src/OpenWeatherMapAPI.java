03d1231c2ce7ab3c36b178c38703d6cb8c24f75c
klov <klov@kth.se>
2023-04-25 13:46:25 +0200

    Changed class to handle String input and give correct output

--- src/OpenWeatherMapAPI.java
+++ src/OpenWeatherMapAPI.java
@@ -5,7 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.ProtocolException;
-import java.util.Scanner;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,54 +15,46 @@ import java.util.regex.Pattern;
  */
 public class OpenWeatherMapAPI {
     private String apiKey = "8257206a615b0b55accb2cf9cce30db0";
-    private String apiWeather;
+    private String apiWeather; 
 
-    public void weatherInformation() {
-        Scanner scanner = new Scanner(System.in);
+    public String weatherInformation(String city) {
+        
+        //This is the standard call method for openweather
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
-        while (true) {
-            System.out.print("Enter a city name (or 'quit' to exit): ");
-            String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("quit")) {
-                break;
-            }
+        try {
+            //Set up an http connection
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
 
-            //This is the standard call method for openweather
-            String url = "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + apiKey;
+            int responseCode = connection.getResponseCode();
+            //HTTP_OK is set to 200 which means it was a successful request for some reason
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
 
-            try {
-                //Set up an http connection
-                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-                connection.setRequestMethod("GET");
-
-                int responseCode = connection.getResponseCode();
-                //HTTP_OK is set to 200 which means it was a successful request for some reason
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String inputLine;
-                    StringBuffer response = new StringBuffer();
-
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-
-                    //Extract weather information using regular expressions
-                    Pattern pattern = Pattern.compile("\"main\":\"(.+?)\",\"description\":\"(.+?)\"");
-                    Matcher matcher = pattern.matcher(response.toString());
-                    if (matcher.find()) {
-                        apiWeather = matcher.group(2);
-                    } 
-                } else {
-                    System.out.println("Error: " + responseCode);
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
                 }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                in.close();
+
+                //Extract weather information using regular expressions
+                Pattern pattern = Pattern.compile("\"main\":\"(.+?)\",\"description\":\"(.+?)\"");
+                Matcher matcher = pattern.matcher(response.toString());
+                if (matcher.find()) {
+                    apiWeather = matcher.group(2); // Will be the description of the weather
+                } 
+            } else {
+                System.out.println("Error: " + responseCode);
             }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return apiWeather;
     }
 }
