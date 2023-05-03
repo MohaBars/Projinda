cad45c9b833dc61fca4c4808dc3a43c0d1bcd230
klov <klov@kth.se>
2023-05-03 10:58:36 +0200

    Added Documentation and fixed a small bug where we searched for the city on spotify instead of the weather

--- src/WeatherHandler.java
+++ src/WeatherHandler.java
@@ -6,7 +6,8 @@ import com.sun.net.httpserver.HttpHandler;
 /**
  * A weather handler for handling HTTP requests to retrieve weather information
  * for a specified city using OpenWeatherMap API, and sending the response in
- * plain text format.
+ * plain text format. The respons in then used with Spotifys API to search for a 
+ * playlist of the name of the OpenWeatherMap API's desription of the weather.
  *
  * The 'handle' method retrieves the query parameters from the incoming HTTP
  * request URL, specifically the 'query' parameter which represents the city
@@ -43,14 +44,19 @@ public class WeatherHandler implements HttpHandler {
             }
         }
 
+        // Gets the weather from input location
         OpenWeatherMapAPI openWeatherMapAPI = new OpenWeatherMapAPI();
-        SpotifyAPI spotify = new SpotifyAPI();
         String[] result = openWeatherMapAPI.weatherInformation(city);
         String weather = result[0];
-        String[] links = spotify.searchPlaylists(city);
+
+        // Uses the output weather desciption to search for a playlist of that name
+        SpotifyAPI spotify = new SpotifyAPI();
+        String[] links = spotify.searchPlaylists(weather);
         String link = links[0];
         String coverLink = links[1];
 
+        // Addresses the the fact that the time seemed to be off by 2 hours. Might be because we have not payed for the API and use
+        // a free version that might not be updated each minute.
         int timeInt = Integer.parseInt(result[1]); // incorrect time by 2 hours.
         timeInt += 2; //Time in hours as an int between 0-23
         String time = Integer.toString(timeInt);
