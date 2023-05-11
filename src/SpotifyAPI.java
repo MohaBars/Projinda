65680ba31ba93b31986db81ef6cc93f2c4471ae5
pittakis <pittakis@kth.se>
2023-05-11 10:22:47 +0200

    Added documentation

--- src/SpotifyAPI.java
+++ src/SpotifyAPI.java
@@ -9,29 +9,48 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SpotifyAPI {
     private String clientId = "4528c15345444a0e867580331f178e08";
     private String clientSecret = "e7fa7fd60a4b4624883614601b2f1fdc";
     private String accessToken;
 
+
+    /**
+     * Constructor for the SpotifyAPI class.
+     * Initializes the access token by calling the getAccessToken() method.
+     */
     public SpotifyAPI() {
         accessToken = getAccessToken();
     }
 
+    /**
+     * Retrieves an access token from the Spotify API using the client ID and client secret.
+     *
+     * @return The access token as a string.
+     */
     private String getAccessToken() {
         try {
+            // Create the URL object for the Spotify token endpoint
             URL url = new URL("https://accounts.spotify.com/api/token");
+            // Open a connection to the URL
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // Set the request method to POST
             connection.setRequestMethod("POST");
+            // Set the request properties
             connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             connection.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString((clientId + ":" + clientSecret).getBytes(StandardCharsets.UTF_8)));
+            // Enable output for writing the request body
             connection.setDoOutput(true);
             OutputStream os = connection.getOutputStream();
+            // Write the grant_type parameter to the request body
             os.write("grant_type=client_credentials".getBytes(StandardCharsets.UTF_8));
             os.flush();
             os.close();
 
+            // Read the response from the connection
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String inputLine;
             StringBuilder content = new StringBuilder();
@@ -49,18 +68,29 @@ public class SpotifyAPI {
         return null;
     }
 
-    public String[] searchPlaylists(String apiWeather) {
-        String query = apiWeather + " playlist";
-        String[] playlistUrls = null;
+    /**
+     * Searches for a playlist related to the provided API weather parameter.
+     *
+     * @param apiWeather The weather parameter used to search for a playlist.
+     * @return An array containing playlist data: [0] playlist URL, [1] artwork URL.
+     */
+    public String[] searchPlaylist(String apiWeather) {
+        // Prepare the query parameter for the Spotify API search
+        String query = apiWeather.replace(" ", "%20") + "%20playlist";
+        String[] playlistData = new String[2];
 
         try {
-            URL url = new URL("https://api.spotify.com/v1/search?q=" + query + "&type=playlist&limit=10");
+            // Create the URL object for the Spotify API search endpoint
+            URL url = new URL("https://api.spotify.com/v1/search?q=" + query + "&type=playlist&limit=1");
+            // Open a connection to the URL
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // Set the request method to GET
             connection.setRequestMethod("GET");
+            // Set the request properties
             connection.setRequestProperty("Accept", "application/json");
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setRequestProperty("Authorization", "Bearer " + accessToken);
-
+            // Read the response from the connection
             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String inputLine;
             StringBuilder content = new StringBuilder();
@@ -70,21 +100,32 @@ public class SpotifyAPI {
             in.close();
             connection.disconnect();
 
-            JSONObject jsonObject = new JSONObject(content.toString());
-            JSONArray playlists = jsonObject.getJSONObject("playlists").getJSONArray("items");
-            int playlistCount = playlists.length();
-            playlistUrls = new String[playlistCount];
+            // Convert the response to a JSON string
+            String jsonString = content.toString();
 
-            for (int i = 0; i < playlistCount; i++) {
-                JSONObject playlist = playlists.getJSONObject(i);
-                String playlistUrl = playlist.getJSONObject("external_urls").getString("spotify");
-                playlistUrls[i] = playlistUrl;
+            // Extract the playlist URL using a regular expression
+            Pattern playlistPattern = Pattern.compile("\"spotify\"\\s*:\\s*\"(https://open\\.spotify\\.com/playlist/[^\"]+)\"");
+            Matcher playlistMatcher = playlistPattern.matcher(jsonString);
+
+            if (playlistMatcher.find()) {
+                // Store the playlist URL in the playlistData array
+                playlistData[0] = playlistMatcher.group(1);
+            }
+
+            // Extract the artwork URL using a regular expression
+            Pattern artworkPattern = Pattern.compile("\"images\"\\s*:\\s*\\[\\s*\\{[^\\}]*\"url\"\\s*:\\s*\"([^\"]+)\"");
+            Matcher artworkMatcher = artworkPattern.matcher(jsonString);
+
+            if (artworkMatcher.find()) {
+                // Store the artwork URL in the playlistData array
+                playlistData[1] = artworkMatcher.group(1);
+
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return playlistUrls;
+        return playlistData;
     }
 }
